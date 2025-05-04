72cc473860a8f01c9eb18f2f80f84b41551438a9
Zhenya Tikhonov <zhenya.tikhonov@octopus.com>
2025-05-04 21:25:47 +0400

    docs: document Logger behavior

--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,17 @@
 class Logger {
+  /**
+   * MUST ONLY be USED for logging results of the command execution,
+   * so that the user could pipe the output to a file or another command.
+   * MUST NOT be used for internal meta logging.
+   */
   stdout(message: string): void {
     process.stdout.write(`${message}\n`);
   }
 
+  /**
+   * Should be used for logging internal meta information,
+   * such as progress, errors, etc.
+   */
   stderr(message: string): void {
     process.stderr.write(`${message}\n`);
   }
