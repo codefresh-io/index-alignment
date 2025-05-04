ddb4937ccbfa8a39dc3b79be9860c54e4b5a0672
Zhenya Tikhonov <zhenya.tikhonov@octopus.com>
2025-05-04 21:19:41 +0400

    docs: document `isIndexEqual()`

--- src/compare.ts
+++ src/compare.ts
@@ -13,6 +13,25 @@ interface CompareOptions {
   dbMap?: DbMapRaw;
 }
 
+/**
+ * Compare two indexes to check if they are equal.
+ *
+ * Algorithm:
+ *
+ *  - The `key` property is an object that contains the fields and their sort order.
+ * The order of the keys in the object DO MATTER, that's why we need to compare
+ * objects including the order of the keys.
+ * `JSON.stringify()` is visiting keys in the same predictable and repeatable order
+ * as `Object.keys()`, that's why it has been chosen.
+ *
+ * - The `name` property is not used in the comparison because it can be arbitrary
+ * and does not affect the index itself.
+ *
+ * - Rest of the properties are compared as they are, regardless of the key order,
+ * that's why we use `deepStrictEqual` ({@link https://nodejs.org/api/assert.html#assertdeepstrictequalactual-expected-message|docs}).
+ *
+ * - If both `key` and options (except for `name`) are equal, the indexes are considered equal.
+ */
 export const isIndexEqual = (a: Index, b: Index): boolean => {
   const aKey = a.key;
   const aOptions = { ...a, key: undefined, name: undefined };
