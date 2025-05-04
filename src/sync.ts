2d6504c65e9278aea350bb440135b380ad91709c
Zhenya Tikhonov <zhenya.tikhonov@octopus.com>
2025-05-04 21:47:32 +0400

    docs: document `syncIndexes()` function

--- src/sync.ts
+++ src/sync.ts
@@ -22,6 +22,42 @@ const getOrCreateCollection = async (db: Db, collectionName: string): Promise<Co
   return db.collection(collectionName);
 };
 
+/**
+ * Syncs indexes for a collection.
+ *
+ * This function must strictly adhere to the requirement of doing as minimal impact on performance as possible.
+ * This means, that in common case we do want to create new indexes first, then drop extra ones.
+ * This is because new indexes may be a replacement for older ones, and dropping older indexes
+ * before creating a replacement will cause degraded performance window.
+ *
+ * We must also consider the upper limit of 64 indexes per collection.
+ * If we're not able to create all missing indexes at once without exceeding the limit,
+ * we need to:
+ * - create as many missing indexes as possible
+ * - drop one extra index
+ * - repeat the process until all missing indexes are created
+ * - drop all extra indexes, if any
+ *
+ * For the sake of this function, "free slots" are the number of indexes
+ * that can be created in a collection at the moment without exceeding the limit of 64 indexes.
+ *
+ * Algorithm:
+ * 1. If there are no missing or extra indexes, RETURN.
+ *    Sync is done.
+ *    Exit case for recursion calls.
+ * 2. If number of missing indexes is greater than the sum of extra indexes and free slots, THROW.
+ *    Sync is impossible.
+ *    This means that even if all extra indexes are dropped,
+ *    there won't be enough free slots to create all missing indexes.
+ *    This is a case when we forced to do manual sync.
+ * 3. If there are no missing indexes, drop all extra indexes. RETURN.
+ *    Sync is done.
+ * 4. If there are no extra indexes, create all missing indexes. RETURN.
+ *    Sync is done.
+ * 5. Create as many missing indexes as possible, filling all free slots.
+ * 6. Drop _one_ extra index, releasing a single slot.
+ * 7. Repeat the process starting from step 1.
+ */
 export const syncIndexes = async (collection: Collection, freeSlots: number, missing: Index[] = [], extra: Index[] = []): Promise<void> => {
   const missingIndexes = [...missing];
   const extraIndexes = [...extra];
@@ -29,8 +65,8 @@ export const syncIndexes = async (collection: Collection, freeSlots: number, mis
   if (missingIndexes.length === 0 && extraIndexes.length === 0) return;
 
   if (missingIndexes.length > extraIndexes.length + freeSlots) {
-    logger.stderr(`Not enough free slots to create ${missingIndexes.length} indexes. Consider manual sync`);
-    throw new Error('Not enough free slots');
+    logger.stderr(`Not enough free slots to create ${missingIndexes.length} indexes, even if all extra ones are dropped. Consider manual sync with the help of support.`);
+    throw new Error('Not enough free slots to create indexes');
   }
 
   if (missingIndexes.length === 0) {
